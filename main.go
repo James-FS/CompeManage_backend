4e423eeb2092332c200213133204c73434f29a2d
bleach <142779648+b1each232425@users.noreply.github.com>
2026-01-20 14:44:47 +0800

    fix:解决冲突

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"CompeManage_backend/config"
 	"CompeManage_backend/database"
 	"CompeManage_backend/middleware"
-	"CompeManage_backend/models"
 	"CompeManage_backend/routes"
 	"log"
 
@@ -23,11 +22,6 @@ func main() {
 
 	// 连接数据库
 	database.Init()
-	database.DB.AutoMigrate(
-		&models.User{},
-		&models.Role{},
-		&models.Permission{},
-	)
 	database.InitData()
 	// 创建Gin引擎（开发环境用gin.Default，生产可改为gin.ReleaseMode）
 	r := gin.Default()
