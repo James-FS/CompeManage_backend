b5c279b54905377e647de6d175855ccb663e2ade
bleach <142779648+b1each232425@users.noreply.github.com>
2026-01-29 10:57:42 +0800

    feat:暂存

--- routes/routes.go
+++ routes/routes.go
@@ -30,7 +30,7 @@ func SetupRoutes(r *gin.Engine) {
 		apiGroup.GET("/notice/list", controllers.GetNoticeList)  // 通知列表+筛选
 		apiGroup.GET("/notice/:id", controllers.GetNoticeDetail) // 单个通知查看
 		apiGroup.POST("/notice/create", controllers.CreateNotice)
-		apiGroup.POST("/comp/notice/create", controllers.CreateCompNotice)
+		apiGroup.POST("/notice/comp/create", controllers.CreateCompNotice)
 		apiGroup.GET("/college/list", controllers.GetCollegeList)
 	}
 
