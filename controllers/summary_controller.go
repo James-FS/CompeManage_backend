587cbd6752e28960ec8c3b24cd4c25f4ed8c956d
bleach <142779648+b1each232425@users.noreply.github.com>
2026-04-25 09:21:43 +0800

    feat:统一错误处理(statistic和summary)

--- controllers/summary_controller.go
+++ controllers/summary_controller.go
@@ -1,3 +1,4 @@
+// summary_controller.go
 package controllers
 
 import (
@@ -5,7 +6,6 @@ import (
 	"CompeManage_backend/models"
 	"CompeManage_backend/utils"
 	"encoding/json"
-	"net/http"
 	"strconv"
 	"time"
 
@@ -403,9 +403,5 @@ func SaveSummary(c *gin.Context) {
 		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"code": 200,
-		"msg":  "保存成功",
-		"data": summary,
-	})
+	utils.SuccessWithMessage(c, "保存成功", summary)
 }
